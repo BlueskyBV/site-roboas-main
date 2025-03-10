cc13a96368d82f4a9102400813854cff2cfca356
BlueskyBV <alexcolojoara007@gmail.com>
2025-03-10 12:37:31 +0200

    final version before natio

--- app.js
+++ app.js
@@ -106,6 +106,39 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+let slideIndex = 1;
+document.addEventListener("DOMContentLoaded", function () {
+    showSlides(slideIndex);
+});
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; } 
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].classList.add("active");
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Function for dot navigation
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 
 
 
